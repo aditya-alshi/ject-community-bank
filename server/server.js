b0fb5c1513e794c50046c2981b36393eaa80f975
Aditya Alshi <alshiaditya55@gmail.com>
2024-01-30 15:17:11 +0530

    build added to server

--- server/server.js
+++ server/server.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const ject = require('./ject-database');
+const path = require('path');
+
 
 const app = express();
 
+
+
 app.use(cors());
 app.use(express.json());
-
-
-app.get('/', (req, res)=>{
-    res.send("This is port 8000")
-})
 
 app.post('/submit', (req, res)=>{
     const formData = req.body;
     ject.addDocument(formData);
 } )
 
+app.use(express.static(path.join('public')));
+app.use((req,res) => {
+   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 app.listen(8000);
